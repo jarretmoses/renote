7ceb5887f428602f67f67e4ba0187fff12cedb01
Fabio Kenji <seibzhen@gmail.com>
2014-12-02 11:13:25 -0500

    added markers js

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,14 +1,39 @@
-
 $(function() {
 
   var video = $("video").get(0);
 
   $("#bookmarks").on("click", ".bookmark", function(event) {
     event.preventDefault();
-    // event.preventDefault();
-    console.log(this)
     video.currentTime = $(this).data('time')
   });
+
+  console.log(videojs)
+
+  var avideojs = videojs('MY_VIDEO_1');
+
+  var markers = $.map($(".bookmark"), function(bookmark) { 
+    return $(bookmark).data('time');
+  });
+
+  var titles = $.map($(".bookmark"), function(bookmark) { 
+    return $(bookmark).text().trim();
+  });  
+
+  avideojs.markers({
+  setting: {
+    markerStyle: {
+      'width':'8px',
+      'background-color': 'red'
+    },
+    markerTip:{
+      display: true,
+      default_text: "",
+      show_colon: false
+    },
+  },
+  marker_breaks: markers,
+  marker_text  : titles
+});  
 
 
   $("#create-bookmark").on('submit', function(event) {
