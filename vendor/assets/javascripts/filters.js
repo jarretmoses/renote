2afa3ffe9ff40c3c68454c9ccbe66788d39d082f
jmoses89 <jarret.moses@gmail.com>
2014-12-04 17:18:54 -0500

    add filters and change video size

---
+++ vendor/assets/javascripts/filters.js
@@ -0,0 +1,27 @@
+  
+/*============================================
+Filter Notes/Bookmarks
+==============================================*/
+$(function(){
+  $('#filter-works a').click(function(e){
+    e.preventDefault();
+    $('#filter-works li').removeClass('active');
+    $(this).parent('li').addClass('active');
+
+    //category = .notes or .bookmarks
+    var category = $(this).attr('data-filter');
+
+    $('.custom-item').each(function(){
+     if($(this).is(category)){
+      $(this).show();
+        // $(this).removeClass('filtered').addClass('selected');
+      }
+      else{
+        $(this).hide();
+        // $(this).removeClass('selected').addClass('filtered');
+      }
+
+    });
+  });
+
+})
