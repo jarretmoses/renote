bc8ab9bf82db419cbfcb5be9d039748994c67917
Fabio Kenji <seibzhen@gmail.com>
2014-12-06 14:40:55 -0500

    removed inline styles, changed name to re.note, removed unused code

---
+++ app/assets/javascripts/screenshot.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  var $screenshotArea = $("<canvas>")
+  $screenshotArea.hide();
+
+  $screenshotArea.attr('width', '400px');
+  $screenshotArea.attr('height', '300px');
+
+  var $page = $("body")
+
+})
