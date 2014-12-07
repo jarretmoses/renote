6471d8a3463ee54971bcf7b812b91c3e66547a7b
Fabio Kenji <seibzhen@gmail.com>
2014-12-07 17:30:30 -0500

    validating length of bookmark title on create

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :video
   belongs_to :user
+
+  validates :start_time, numericality: true
+  validates :title, length: { maximum: 255 }, unless: Proc.new { |a| a.title.nil? }
+
 end
