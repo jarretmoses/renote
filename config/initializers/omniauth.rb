32ae3ebeee4c848ed680cf305dca28ef31744148
Jarret Moses <jarret.moses@gmail.com>
2014-12-07 18:08:10 -0500

    add evernote gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :evernote, ENV['EVERNOTE_KEY'], ENV['EVERNOTE_SECRET'], :client_options => { :site => 'https://sandbox.evernote.com' }
 end
