9b2042846a19d39c34f79ca18a27b6cec37aa2ad
natekratchman <natecrashman@gmail.com>
2014-12-09 09:34:09 -0500

    update opentip hide logic, prevent bookmark input field from emptying on error

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -12,6 +12,18 @@ $(function() {
 
   player.render();
 
+  $('#title').on('click', function(){
+    title = $('#title').text().trim();
+    // append (use form_for, make dynamic)
+      // <form accept-charset="UTF-8" action="/videos/3" class="edit_video" data-remote="true" id="edit_video_3" method="post" _lpchecked="1"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="_method" type="hidden" value="patch"></div>
+      // <div class="form-group" id="no-title">
+      // <input class="form-control input-lg" id="video_title" name="video[title]" type="text">
+      // </div>
+      // </form>
+    // to
+      // <div id="title" class="col-md-8">
+  })
+  
 
   // ToolTip
   if($("#video_title").length > 0) {
